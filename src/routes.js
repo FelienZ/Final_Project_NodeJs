1c2d8426231e384fc2c14f127fbdde1649a5420e
Rajab <rajabfadh@gmail.com>
2025-06-26 16:24:23 +0700

    Final Project Backend  NodeJs dicoding

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const { addBooks, getAllBooks, getBooksById, updateBookById, deleteBookById } = require('./requestHandler');
+
+const routes = ([
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooks
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooks
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBooksById
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: updateBookById
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookById
+  },
+
+]);
+
+module.exports = routes;
